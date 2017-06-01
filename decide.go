98fabd8919bbc3ac5c28875f7f6b3ce52d47f550
ironlung975 <mleung975@gmail.com>
2017-06-01 11:57:41 -0700

    Have the tree build off of the priority values first so they are guaranteed to be listed at root

--- decide.go
+++ decide.go
@@ -20,23 +20,47 @@ func NewTree(objects, priority map[string][]string) (*Tree, error) {
 
 	objectSorter := NewFrequencySorter()
 	expressionSorter := NewFrequencySorter()
+	prioritySorter := NewFrequencySorter()
 
 	// Build up frequency tables
 	for object, list := range objects {
+		var priorityInList bool
 		for _, expString := range list {
 			expressionSorter.AddToFrequencies(expString)
 		}
+
+		// Do a sort on the objects with priority expressions first
+		if priorityInList {
+			prioritySorter.AddValue(object, len(list)+len(priority[object]))
+			continue
+		}
+
+		// Do not need to sort objects that do not have priority expressions if they are already in the priority sort
 		objectSorter.AddValue(object, len(list))
 	}
 
 	var root *Node
 	var err error
-	for _, object := range objectSorter.FrequencyList() {
+
+	// Add the nodes with the priority expressions (which are sorted) first
+	for _, object := range prioritySorter.FrequencyList() {
 		expressions := objects[object]
+		priorityExpresions := priority[object]
 		expressionSorter.SortReverse(expressions)
 
 		// prepend the expressions with the priority values so they are built first
 		expressions = append(priority[object], expressions...)
+
+		root, err = addNode(root, expressions, object)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// Add the rest of the nodes
+	for _, object := range objectSorter.FrequencyList() {
+		expressions := objects[object]
+		expressionSorter.SortReverse(expressions)
 
 		root, err = addNode(root, expressions, object)
 		if err != nil {
